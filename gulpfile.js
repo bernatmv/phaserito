367e8cfad7f01949cb05e853dfc62253a5627b29
Abraham Alcaina <abraham.alcaina@gmail.com>
2015-01-27 16:14:08 +0100

    (feat) travis configuration

--- gulpfile.js
+++ gulpfile.js
@@ -122,3 +122,11 @@ gulp.task('tdd', function(done) {
 		autoWatch: true
 	}, done);
 });
+
+gulp.task('travis', function(done) {
+	karma.start({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true,
+		browsers: ['Firefox']
+	}, done);
+});
