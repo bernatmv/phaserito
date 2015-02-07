7e87408642adf1cc7b001e0d11960f1b8737ae27
Bernat Martinez <bernatmv@gmail.com>
2015-02-07 20:22:20 +0100

    (feat) Added documentation to the manager and new assets for the sound and music mute buttons

--- src/lib/components/soundManager/index.js
+++ src/lib/components/soundManager/index.js
@@ -54,6 +54,7 @@ export class SoundManager {
 			music: {
 				group: undefined,
 				volume: 10,
+				loop: false,
 				on: {},
 				off: {},
 				sprites: {
@@ -149,48 +150,82 @@ export class SoundManager {
 			return this;
 	}
 
-	// TODO:
-	// Add the option to set a mute button as standalone for sound and another for music
-
+	/**
+	 * Set the volume for either the global game or just the music
+	 * @param {String} options.type:   type   	The volume to change, possible values are "fx", which is the general volume and the default value, or "music"
+	 * @param {Number} options.volume: volume 	The volume based on the steps defined (a 10 on a 10-steps volume would be the normal 100% volume), default value is 10
+	 * @return {SoundManager}					Returns this for piping
+	 */
 	setVolume({type: type = 'fx', volume: volume = 10}) {
 		return (type === 'fx') ? this.__setFxVolume(volume) : this.__setMusicVolume(volume);
 	}
 
+	/**
+	 * Sets the global volume (privately called)
+	 * @param  {Number} volumeStep The volume based on the steps defined (a 10 on a 10-steps volume would be the normal 100% volume)
+	 * @return {SoundManager}      Return this for piping
+	 */
 	__setFxVolume(volumeStep) {
 		this.bar.fx.volume = volumeStep;
 		this.__displayVolumeBar('fx');
 		// this volume affects the whole application, including the music
 		this.game.sound.volume = this.__getFxVolume();
+		// return this for piping
+		return this;
 	}
 
+	/**
+	 * Sets the music volume (privately called)
+	 * @param  {Number} volumeStep The volume based on the steps defined (a 10 on a 10-steps volume would be the normal 100% volume)
+	 * @return {SoundManager}      Return this for piping
+	 */
 	__setMusicVolume(volumeStep) {
 		this.bar.music.volume = volumeStep;
 		this.__displayVolumeBar('music');
 		// this volume affects only the music
 		this.currentLap && (this.currentLap.volume = this.__getMusicVolume());
+		// return this for piping
+		return this;
 	}
 
+	/**
+	 * Get the actual volume that Phaser needs (0 .. 1), uses the actual step and the numbers of steps to calculate it
+	 * @return {Number} Return the volume in a Phaser-like fashion, value between 0 .. 1
+	 */
 	__getFxVolume() {
 		return (this.bar.fx.volume / this.bar.fx.sprites.fore.length).toFixed(2);
 	}
 
+	/**
+	 * Get the actual music volume that Phaser needs (0 .. 1), uses the actual step and the numbers of steps to calculate it
+	 * @return {Number} Return the volume in a Phaser-like fashion, value between 0 .. 1
+	 */
 	__getMusicVolume() {
 		return (this.bar.music.volume / this.bar.music.sprites.fore.length).toFixed(2);
 	}
 
-	// add possibility to pass one lap with or w/o loop
-	// multiple laps with or w/o general loop
-	// music play and resume
-
+	/**
+	 * Used to define the music audiosprite, tracks and if it should loop
+	 * @param {Array}   options.laps: laps Define the tracks to be used in the music, it must be an array with the name of the markers in the audiosprite, eg: ["nameTrack1", "nameTrack2"]
+	 * @param {String}  options.key:  key  The key of the audiosprite, default value is "music"
+	 * @param {Boolean} options.loop: loop Whether music should loop or not, default value is FALSE
+	 * @return {SoundManager} Return this for piping
+	 */
 	setMusic({laps: laps = [], key: key = 'music', loop: loop = false}) {
 		// load audiosprite
 		this.music = this.game.add.audioSprite(key);
 		// save laps
 		this.laps = laps;
+		// save if it should loop or not
+		this.bar.music.loop = loop;
 		// return this for piping
 		return this;
 	}
 
+	/**
+	 * Start playing the music defined in the setMusic method
+	 * @return {SoundManager} Return this for piping
+	 */
 	playMusic() {
 		if (this.laps.length > 0) {
 			// get first lap in the collection
@@ -198,8 +233,8 @@ export class SoundManager {
 			// play lap
 			this.currentLap = this.music.play(lapName);			
 			this.currentLap.volume = this.__getMusicVolume();
-			// push the lap at the end of the collection
-			this.laps.push(lapName);
+			// push the lap at the end of the collection if it should loop
+			this.bar.music.loop && this.laps.push(lapName);
 			// when the lap finishes, play the next one
 			if (!this.currentLap.onStop.has(this.playMusic, this)) {
 				this.currentLap.onStop.add(this.playMusic, this);
@@ -209,11 +244,27 @@ export class SoundManager {
 		return this;
 	}
 
+	/**
+	 * Pauses the music
+	 * @return {SoundManager} Return this for piping
+	 */
 	pauseMusic() {
 		this.currentLap && this.currentLap.pause();
+		// return this for piping
+		return this;
 	}
 
+	/**
+	 * Resumes the music
+	 * @return {SoundManager} Return this for piping
+	 */
 	resumeMusic() {
 		this.currentLap && this.currentLap.resume();
+		// return this for piping
+		return this;
 	}
+
+	// TODO:
+	// Add the option to set a mute button as standalone for sound and another for music
+
 }
